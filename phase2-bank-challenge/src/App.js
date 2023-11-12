e928ae3050c873218463154b51243db5f7626436
terence <terenceshawn18@outlook.com>
2023-11-12 12:50:48 +0300

    added comments to app.js

--- phase2-bank-challenge/src/App.js
+++ phase2-bank-challenge/src/App.js
@@ -1,17 +1,22 @@
+// Importing data from other components
 import React, { useState, useEffect } from 'react';
 import TransactionTable from './TransactionTable';
 import TransactionForm from './TransactionForm';
 import SearchBar from "./SearchBar"
 import './App.css'
 
+// Main component for the application
 const App = () => {
+  // this is to store transactions and search terms
   const [transactions, setTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
+  // Fetch transactions from the server when the component mounts
   useEffect(() => {
     fetchTransactions();
   }, []);
 
+  // Function to fetch transactions from the db.json file
   const fetchTransactions = async () => {
     try {
       const response = await fetch('http://localhost:8001/transactions');
@@ -19,6 +24,7 @@ const App = () => {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
+      // change the response into JSON and set the transactions in the state
       const data = await response.json();
       const transactionsArray = data.transactions || data;
 
@@ -28,14 +34,17 @@ const App = () => {
     }
   };
 
+  // Function to handle adding a new transaction
   const handleAddTransaction = (newTransaction) => {
+    // Update the transactions state with the new transaction
     setTransactions([...transactions, newTransaction]);
   };
 
+  // Filter transactions based on the search term
   const filteredTransactions = transactions.filter((transaction) =>
     transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
+// components will be loaded in the return of the function
   return (
     <div id='parentdiv'>
       <div id='Siteheader'><h1>Flat ironBank</h1></div>
@@ -46,4 +55,6 @@ const App = () => {
   );
 };
 
+// Export the main App component to the index.js file
 export default App;
+
